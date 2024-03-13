d87e08dd26380ab96406b37046c3d5c505278d53
Duha YILDIRIM <57143165+duhayildirim@users.noreply.github.com>
2024-03-14 02:45:06 +0300

    bug fix

--- containers/home/index.tsx
+++ containers/home/index.tsx
@@ -1,17 +1,53 @@
 import React from "react";
 
-import Movies from "@/mocks/movies.json";
-import Genres from "@/mocks/genres.json";
+import MoviesData from "@/mocks/movies.json";
+import GenresData from "@/mocks/genres.json";
 import FeaturedMovie from "@/components/featured-movie";
 import Categories from "@/components/categories";
 import MoviesSection from "@/components/movies-section";
+
+
+interface Movie {
+  adult: boolean;
+  backdrop_path: string;
+  genre_ids: number[];
+  id: number;
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  release_date: string;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+}
+
+interface Genre {
+  id: number;
+  name: string;
+}
+
+interface GenresResponse {
+  genres: Genre[];
+}
+
+interface MoviesResponse {
+  page: number;
+  results: Movie[];
+}
+
+const Movies: MoviesResponse = MoviesData;
+const Genres: GenresResponse = GenresData;
 
 function HomeContainer() {
   return (
     <div>
       <FeaturedMovie movie={Movies.results[0]} />
       <Categories categories={Genres.genres.slice(0, 5)} />
-      <MoviesSection title="Popular Films" movies={Movies.results.slice(1,7)} />
+      <MoviesSection title="Popular Films" movies={Movies.results.slice(1, 7)} />
+      <MoviesSection title="Your Favorites" movies={Movies.results.slice(7, 13)} />
     </div>
   );
 }
